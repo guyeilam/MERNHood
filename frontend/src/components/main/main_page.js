32dd0e802940f2b16f0306a63b55a3faa163a482
Guy Eilam <guyeilam@gmail.com>
2019-04-01 14:19:13 -0400

    Add user auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>my MERN stack app</h1>
+        <footer>
+            footer
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import session from './session_reducer';
+import session from './session_api_reducer';
+import errors from './errors_reducer';
 
 const RootReducer = combineReducers({
-  session
+  session,
+  errors
 });
 
 export default RootReducer;
