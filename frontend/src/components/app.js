8330f821c234d964f35ba39ec5cb0c6551ecabf0
Luis Ball <luqven@gmail.com>
2019-04-02 13:36:10 -0400

    clean up conflicts

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -6,10 +6,7 @@ import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
-<<<<<<< HEAD
-=======
 import ComponentLibrary from './lib/lib';
->>>>>>> 34b258435a000a0c36598ff350ca55e3281614fc
 
 const App = () => (
   <div>
@@ -19,7 +16,6 @@ const App = () => (
         <AuthRoute exact path="/lib" component={ComponentLibrary} />
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
-        <AuthRoute exact path="/lib" component={Library} />
     </Switch>
   </div>
 );
