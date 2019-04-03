c0c379efd59249af3ba1fa74a7b1d755768502fe
Luis Ball <luqven@gmail.com>
2019-04-03 12:17:42 -0400

    use gestalts sticky component for styling

--- frontend/src/components/lib/lib.js
+++ frontend/src/components/lib/lib.js
@@ -1,5 +1,6 @@
 // Component examples library
 import React from 'react';
+import { Box, Text, Sticky } from 'gestalt';
 import 'gestalt/dist/gestalt.css';
 import './library.css';
 import DropDown from './dropdown';
@@ -12,58 +13,88 @@ export default class Library extends React.Component {
     super(props);
     this.state = {}
   }
+  // //////////// -- Note --
+  // Box, Text, Sticky from gestalt used here
+  // but are not needed to use DropDown, ColorButton,
+  // LightGreenLink, ColorLink, or Logo components elsewhere. 
+  // 
+  // They are ready to use after import ;)
+  // ////////////
 
   render() {
     return (
       <>
-        {/* Logos */}
-        <section>
-          <h1>Logos</h1>
-          <p>small</p>
-          <Logo size='sm' type='day' />
-          <Logo size='sm' type='night'/>
-          <p>medium</p>
-          <Logo size='md' type='day' />
-          <Logo size='md' type='night' />
-          <p>large</p>
-          <Logo size='lg' type='day' />
-          <Logo size='lg' type='night' />
-        </section>
-
-        {/* Dropdowns */}
-        <section>
-          <h1>Dropdowns</h1>
-          <DropDown
-            title={"Hover Here"}
-            text={['link1', 'link2', 'link3']}
-            links={['#', '#', '#']} />
-          <DropDown
-            title={"Another Dropdown"}
-            text={['link3', 'link4', 'link5']}
-            links={['#', '#', '#']} />
-        </section>
-        <br></br>
-        {/* Buttons */}
-        <section>
-          <h1>Buttons</h1>
-          {/* colors and hover */}
-          <ColorButton text="light green button" padding={2} inline={true} color='green' />
-          <ColorButton text="dark green button" padding={2} inline={true} color='green' hue='dark' />
-          <ColorButton text="custom color button" padding={2} inline={true} color='red' />
-          <ColorButton text="hover color button" padding={2} inline={true} color='gray' hover={true} />
-          <ColorButton text="transparent button" padding={2} inline={true} color='transparent' hover={true} />
-          {/* sizes */}
-          <ColorButton text="small button" padding={2} inline={true} color='green' size='sm' />
-          <ColorButton text="medium button" padding={2} inline={true} color='green' size='md' />
-          <ColorButton text="large button" padding={2} inline={true} color='green' size='lg' />
-        </section>
-
-        {/* Links */}
-        <section>
-          <h1>Links</h1>
-          <LightGreenLink text="lightGreen.com" padding={2} />
-          <ColorLink text="customColor.com" padding={2} color='blue' />
-        </section>
+        <Box color="white" height={'80vh'} overflow="scroll" marginTop={"10"} marginLeft={"5"}>
+          {/* Logos */}
+          <Box>
+            <section>
+              <Sticky top={0}>
+                <Box alignItems="center" color="white" display="flex" height={40}>
+                  <Text bold>Logos</Text>
+                </Box>
+              </Sticky>
+              <p>small</p>
+              <Logo size='sm' type='day' />
+              <Logo size='sm' type='night' />
+              <p>medium</p>
+              <Logo size='md' type='day' />
+              <Logo size='md' type='night' />
+              <p>large</p>
+              <Logo size='lg' type='day' />
+              <Logo size='lg' type='night' />
+            </section>
+          </Box>
+          {/* Dropdowns */}
+          <Box>
+            <section>
+              <Sticky top={0} dangerouslySetZIndex={{ __zIndex: 3 }}>
+                <Box alignItems="center" color="white" display="flex" height={40}>
+                  <Text bold>Dropdowns</Text>
+                </Box>
+              </Sticky>
+              <DropDown
+                title={"Hover Here"}
+                text={['link1', 'link2', 'link3']}
+                links={['#', '#', '#']} />
+              <DropDown
+                title={"Another Dropdown"}
+                text={['link3', 'link4', 'link5']}
+                links={['#', '#', '#']} />
+            </section>
+          </Box>
+          {/* Buttons */}
+          <Box>
+            <section>
+              <Sticky top={0} dangerouslySetZIndex={{ __zIndex: 3 }}>
+                <Box alignItems="center" color="white" display="flex" height={40}>
+                  <Text bold>Buttons</Text>
+                </Box>
+              </Sticky>
+              {/* colors and hover */}
+              <ColorButton text="light green button" padding={2} inline={true} color='green' />
+              <ColorButton text="dark green button" padding={2} inline={true} color='green' hue='dark' />
+              <ColorButton text="custom color button" padding={2} inline={true} color='red' />
+              <ColorButton text="hover color button" padding={2} inline={true} color='gray' hover={true} />
+              <ColorButton text="transparent button" padding={2} inline={true} color='transparent' hover={true} />
+              {/* sizes */}
+              <ColorButton text="small button" padding={2} inline={true} color='green' size='sm' />
+              <ColorButton text="medium button" padding={2} inline={true} color='green' size='md' />
+              <ColorButton text="large button" padding={2} inline={true} color='green' size='lg' />
+            </section>
+          </Box>
+          {/* Links */}
+          <Box>
+            <section>
+              <Sticky top={0} dangerouslySetZIndex={{ __zIndex: 3 }}>
+                <Box alignItems="center" color="white" display="flex" height={40}>
+                  <Text bold>Links</Text>
+                </Box>
+              </Sticky>
+              <LightGreenLink text="lightGreen.com" padding={2} />
+              <ColorLink text="customColor.com" padding={2} color='blue' />
+            </section>
+          </Box>
+        </Box>
       </>
     )
   }
