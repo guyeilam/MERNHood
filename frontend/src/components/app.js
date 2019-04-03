b893bbbbf32752a54a07f45f9c2c672005f6e6be
Guy Eilam <guyeilam@gmail.com>
2019-04-03 13:33:26 -0400

    Add CSS reset

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,7 +7,7 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 
-import './root.css';
+import './reset.css';
 
 const App = () => (
   <div>
