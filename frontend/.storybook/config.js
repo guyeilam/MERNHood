232b3f6e1b5cde3e3195f8a6d359c170889e58b7
Luis H. Bajj Jr <luqven@gmail.com>
2019-04-19 11:01:46 -0400

    fix styles imports for story book

--- frontend/.storybook/config.js
+++ frontend/.storybook/config.js
@@ -6,6 +6,8 @@ import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
 
 import '../src/index.css';
+import '../src/components/reset.css';
+import '../src/components/main.css';
 
 const req = requireContext('../src/components', true, /\.stories\.js$/);
 
