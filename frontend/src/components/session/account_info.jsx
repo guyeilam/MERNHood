cb0e83ec67cf86d8ddd4e91e3e8c088a4e4cc759
geim <guyeilam@gmail.com>
2019-04-22 12:15:09 -0400

    Refactor AccountInfoForm component to use Hooks

--- frontend/src/components/session/account_info.jsx
+++ frontend/src/components/session/account_info.jsx
@@ -1,101 +1,84 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import {connect} from 'react-redux';
 
-class AccountInfoForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      firstName: this.props.fieldValues.firstName,
-      lastName: this.props.fieldValues.lastName,
-      email: this.props.fieldValues.email,
-      password: this.props.fieldValues.password,
-      password2: this.props.fieldValues.password2,
-    };
-    this.update = this.update.bind(this);
-    this.renderErrors = this.renderErrors.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function AccountInfoForm({fieldValues, saveValues}) {
+  const [firstName, setFirstName] = useState(fieldValues.firstName);
+  const [lastName, setLastName] = useState(fieldValues.lastName);
+  const [email, setEmail] = useState(fieldValues.email);
+  const [password, setPassword] = useState(fieldValues.password);
+  const [password2, setPassword2] = useState(fieldValues.password2);
 
-  update(field) {
-    return e =>
-      this.setState({
-        [field]: e.currentTarget.value,
-      });
-  }
-
-  handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    const fieldValues = Object.assign({}, this.state);
-    this.props.saveValues(fieldValues);
+    const fieldValues = Object.assign({}, {firstName, lastName, email, password, password2});
+    saveValues(fieldValues);
   }
 
-  renderErrors() {}
+  function renderErrors() {}
 
-  render() {
-    return (
-      <div className="signup-form-main">
-        <div className="signup-form-header-text">Make Your Money Move</div>
-        <div className="signup-form-subheader">
-          Robinhood lets you invest in companies you love, commission-free.
-        </div>
-        <div className="signup-form">
-          <form onSubmit={e => this.handleSubmit(e)}>
-            <div className="signup-form">
-              <div className="signup-form-inputs">
-                <div className="signup-form-inputs-row">
-                  <input
-                    type="text"
-                    className="signup-form-input"
-                    value={this.state.firstName}
-                    onChange={this.update('firstName')}
-                    placeholder="First name"
-                  />
-                  <input
-                    type="text"
-                    className="signup-form-input"
-                    value={this.state.lastName}
-                    onChange={this.update('lastName')}
-                    placeholder="Last name"
-                  />
-                </div>
-                <div className="signup-form-inputs-row">
-                  <input
-                    type="text"
-                    className="signup-form-input"
-                    value={this.state.email}
-                    onChange={this.update('email')}
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="signup-form-inputs-row">
-                  <input
-                    type="password"
-                    className="signup-form-input"
-                    value={this.state.password}
-                    onChange={this.update('password')}
-                    placeholder="Password"
-                  />
-                </div>
-                <div className="signup-form-inputs-row">
-                  <input
-                    type="password"
-                    className="signup-form-input"
-                    value={this.state.password2}
-                    onChange={this.update('password2')}
-                    placeholder="Confirm Password"
-                  />
-                </div>
-                <div className="signup-form-inputs-row">
-                  <input type="submit" className="signup-form-submit" value="Continue" />
-                </div>
-                {this.renderErrors()}
-              </div>
-            </div>
-          </form>
-        </div>
+  return (
+    <div className="signup-form-main">
+      <div className="signup-form-header-text">Make Your Money Move</div>
+      <div className="signup-form-subheader">
+        Robinhood lets you invest in companies you love, commission-free.
       </div>
-    );
-  }
+      <div className="signup-form">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div className="signup-form">
+            <div className="signup-form-inputs">
+              <div className="signup-form-inputs-row">
+                <input
+                  type="text"
+                  className="signup-form-input"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.currentTarget.value)}
+                  placeholder="First name"
+                />
+                <input
+                  type="text"
+                  className="signup-form-input"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.currentTarget.value)}
+                  placeholder="Last name"
+                />
+              </div>
+              <div className="signup-form-inputs-row">
+                <input
+                  type="text"
+                  className="signup-form-input"
+                  value={email}
+                  onChange={(e) => setEmail(e.currentTarget.value)}
+                  placeholder="Email"
+                />
+              </div>
+              <div className="signup-form-inputs-row">
+                <input
+                  type="password"
+                  className="signup-form-input"
+                  value={password}
+                  onChange={(e) => setPassword(e.currentTarget.value)}
+                  placeholder="Password"
+                />
+              </div>
+              <div className="signup-form-inputs-row">
+                <input
+                  type="password"
+                  className="signup-form-input"
+                  value={password2}
+                  onChange={(e) => setPassword2(e.currentTarget.value)}
+                  placeholder="Confirm Password"
+                />
+              </div>
+              <div className="signup-form-inputs-row">
+                <input type="submit" className="signup-form-submit" value="Continue" />
+              </div>
+              {renderErrors()}
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state, ownProps) => {
