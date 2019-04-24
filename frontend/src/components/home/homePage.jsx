0b0fad68c8de4f0f055979731b42bffa25046570
Luis H. Bajj Jr <luqven@gmail.com>
2019-04-24 16:03:47 -0400

    create home grid skeleton

--- frontend/src/components/home/homePage.jsx
+++ frontend/src/components/home/homePage.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import Box from "../library/box";
+import styled from "styled-components";
 
 export default function HomePage() {
+  const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 60vw 40vw;
+    grid-template-rows: 100px 100px 100px;
+    grid-column-gap: 30px;
+    grid-row-gap: 15px;
+  `;
+
+  const styles = {
+    column1: {
+      gridColumn: "1"
+    },
+    column2: {
+      gridColumn: "2"
+    }
+  };
+
   return (
-    <Box>
-      <h1>Home Page</h1>
-    </Box>
+    <Grid>
+      <section style={styles.column1} className="chart-container">
+        <p>chart</p>
+      </section>
+      <section style={styles.column2} className="watching-container">
+        <p>watching</p>
+      </section>
+      <section style={styles.column1} className="top-movers-container">
+        <p>top movers</p>
+      </section>
+      <section style={styles.column1} className="news-container">
+        <p>news</p>
+      </section>
+    </Grid>
   );
 }
